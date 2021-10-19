4a14e437add57a6f39a0f210e70e090e1ac17c7d
Solmaz <44635076+Solymaz@users.noreply.github.com>
2021-10-19 20:15:49 +0200

    nested obj in gql

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,5 +1,11 @@
-const Query = {
-  greeting: () => "Hello world!",
-};
+const db = require("./db");
 
-module.exports = { Query };
+//the resolvers must mirrot the schema
+const Query = {
+  jobs: () => db.jobs.list(),
+};
+//here we are returning the company whose id is the same as companyId of this job
+const Job = {
+  company: (job) => db.companies.get(job.companyId),
+};
+module.exports = { Query, Job };
