bb04b82f43b3be1d65150c04c8076f000f402ed3
Solmaz <44635076+Solymaz@users.noreply.github.com>
2021-10-21 09:54:35 +0200

    fetch a job by Its D
    add argument to schema def,
    handle argument in the resolver function

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,7 +1,12 @@
 const db = require("./db");
 
 //the resolvers must mirrot the schema
+//resolver is an object
+//the first argument to a resolver function is the parent value
+//for job since we are in the Query type that is the root obj and that is why we call the argument root
 const Query = {
+  job: (root, args) => db.jobs.get(args.id),
+  //job: (root, {id}) => db.jobs.get(id),
   jobs: () => db.jobs.list(),
 };
 //here we are returning the company whose id is the same as companyId of this job
